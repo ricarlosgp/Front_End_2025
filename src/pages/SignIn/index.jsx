9db4a8efe9f1f6c8df2a77e012a42a0d4dffd175
Ricarlos Guimarães <ricarlosgp@gmail.com>
2024-09-16 15:33:01 -0300

    Tentando ignorar node_modules que já existe em github

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,43 @@
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+import { Container, Form, Background, TimbirasProfile, SemusProfile } from './styles';
+
+
+export function SignIn() {
+    return (
+        <Container>
+            <Form>
+                <h1>SEMUS</h1>
+                <p>Secretaria Municipal de Saúde</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar" /> 
+
+                <a href="#">
+                    Criar conta
+                </a>    
+            </Form>
+            
+            <Background>
+                <TimbirasProfile> </TimbirasProfile>
+                <SemusProfile> </SemusProfile>
+            </Background>
+        </Container>
+    );
+}
